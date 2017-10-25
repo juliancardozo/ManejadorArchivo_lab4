ec4565be6181dd5423696b52b9b7b8ed8a7709ed
julian_cardozo <julian@pambumedia.com>
2017-10-25 18:42:45 -0300

    versionInicial
    Version Inicial

---
+++ ManejadorArchivo_lab4/ManejadorArchivos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManejadorArchivo_lab4
+{
+    class ManejadorArchivos
+    {
+    }
+}
